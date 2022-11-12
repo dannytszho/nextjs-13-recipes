e5c87ad90fc05bc68d443b0d6c8f2da77af0f581
dannytszho <dannytszho@gmail.com>
2022-11-12 15:50:00 -0600

    Add mock blog data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
 export default function HomePage() {
-    return <div>HomePage</div>
+    return (
+        <div className="flex flex-col m-5 sm:m-10 md:m-28 lg:mx-64">
+            <div>
+                <h1 className="text-5xl">Healthcare Tips</h1>
+                <p className="md:mr-40 lg:mr-60">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            <div>blogPage carusol</div>
+        </div>
+    )
 }
